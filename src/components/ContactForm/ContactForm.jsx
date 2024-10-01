bb1cb0bbb8444a31ec5152390e842ab5b42ce96f
threedev3 <thdev4.1@gmail.com>
2024-10-01 11:56:15 +0500

    Contact us page responsiveness

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -86,26 +86,26 @@ const ContactForm = ({ firstString, secondString, tagLine }) => {
           {
             isContactPage && (
               <div className="flex flex-col gap-8">
-                <div className="flex flex-row gap-6 items-center">
-                  <MapPinIcon width={36} color="#008fbf" />
-                  <div className="flex flex-col gap-2 max-w-sm">
-                    <h3 className="text-headingColor text-[22px]">Address</h3>
-                    <p className="text-headingColor text-base">ASE Global LLC-FZ Business Centre, Meydan Hotel,
+                <div className="flex flex-row xl:gap-6 lg:gap-3 sm:gap-6 gap-6 items-center">
+                  <MapPinIcon className="xl:w-10 lg:w-10 sm:w-8 min-[540px]:w-8 w-11" color="#008fbf" />
+                  <div className="flex flex-col gap-2 ">
+                    <h3 className="text-headingColor xl:text-[22px] lg:text-xl sm:text-[22px] text-lg">Address</h3>
+                    <p className="text-headingColor xl:text-base lg:text-sm sm:text-base text-sm">ASE Global LLC-FZ Business Centre, Meydan Hotel,
                     Nad Al Sheeba, Dubai, UAE</p>
                   </div>
                 </div>
-                <div className="flex flex-row gap-6 items-center">
-                  <PhoneIcon width={36} color="#008fbf" />
-                  <div className="flex flex-col gap-2 max-w-sm">
-                    <h3 className="text-headingColor text-[22px]">Call Us</h3>
-                    <p className="text-headingColor text-base">+971563511722</p>
+                <div className="flex flex-row xl:gap-6 lg:gap-3 sm:gap-6 gap-6 items-center">
+                  <PhoneIcon className="xl:w-10 lg:w-6 sm:w-8 w-6" color="#008fbf" />
+                  <div className="flex flex-col gap-2 ">
+                    <h3 className="text-headingColor xl:text-[22px] lg:text-xl sm:text-[22px] text-lg">Call Us</h3>
+                    <p className="text-headingColor xl:text-base lg:text-sm sm:text-base text-sm">+971563511722</p>
                   </div>
                 </div>
-                <div className="flex flex-row gap-6 items-center">
-                  <EnvelopeIcon width={36} color="#008fbf" />
-                  <div className="flex flex-col gap-2 max-w-sm">
-                    <h3 className="text-headingColor text-[22px]">Email Us</h3>
-                    <p className="text-headingColor text-base">hello@tuitionhighway.com</p>
+                <div className="flex flex-row xl:gap-6 lg:gap-3 sm:gap-6 gap-6 items-center">
+                  <EnvelopeIcon className="xl:w-10 lg:w-6 sm:w-8 w-6" color="#008fbf" />
+                  <div className="flex flex-col gap-2 ">
+                    <h3 className="text-headingColor xl:text-[22px] lg:text-xl sm:text-[22px] text-lg">Email Us</h3>
+                    <p className="text-headingColor xl:text-base lg:text-sm sm:text-base text-sm">hello@tuitionhighway.com</p>
                   </div>
                 </div>
               </div>
